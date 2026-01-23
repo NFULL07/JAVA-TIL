9dc1973c4bd1e3521e8842a22d73d0cbd56168c9
NFULL07 <kjoun1108@gmail.com>
2026-01-23 16:22:14 +0900

    자바의 로컬 변수 초기화(Local Variable Initialization) 규칙: 변수를 초기화하지 않으면 오류 발생

--- src/src/chapter02/VarInitTest.java
+++ src/src/chapter02/VarInitTest.java
@@ -1,3 +1,7 @@
+/* * [에러 연습] variable index might not have been initialized
+ * 원인: 지역 변수 index를 초기화하지 않고 출력(System.out.println)하려 함.
+ * 해결: int index = 0; 처럼 초기값을 주어야 함.
+
 package chapter02;
 
 public class VarInitTest {
@@ -7,4 +11,4 @@ public class VarInitTest {
         index = index + 1;
         System.out.println("index : " + index);
     }
-}
+} */
