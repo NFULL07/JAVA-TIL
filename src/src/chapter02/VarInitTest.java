11e9b7d5f9b68c7be077b7c7a28bd9ce6d4c2bbe
NFULL07 <kjoun1108@gmail.com>
2026-01-23 15:04:12 +0900

    자바의 로컬 변수 초기화(Local Variable Initialization) 규칙: 변수를 초기화하지 않으면 오류 발생

---
+++ src/src/chapter02/VarInitTest.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class VarInitTest {
+    static void main(String[] args) {
+        int index;
+
+        index = index + 1;
+        System.out.println("index : " + index);
+    }
+}
