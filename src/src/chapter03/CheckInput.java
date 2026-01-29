440f217de1c7224d55a8b2c2b14ca454467e0bbd
NFULL07 <kjoun1108@gmail.com>
2026-01-29 23:59:57 +0900

    DoWhile문응용

---
+++ src/src/chapter03/CheckInput.java
@@ -0,0 +1,13 @@
+package chapter03;
+import java.util.Scanner;
+
+public class CheckInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int month;
+        do {
+            System.out.print("올바른 월을 입력하시오 [1-12]: ");
+            month = input.nextInt();
+        }   while (month < 1 || month > 12);
+    }
+}
